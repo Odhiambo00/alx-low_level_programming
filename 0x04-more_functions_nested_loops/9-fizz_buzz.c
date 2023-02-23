ab123d707478b40e5c5b6d3d9642a373cf6d32e4
elias <odhiamboelias99@gmail.com>
2023-02-23 11:57:54 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,40 +1,63 @@
-#include "main.h"
 #include <stdio.h>
-#include <math.io>
+#include <math.h>
+#include "main.h"
 
 /**
- *  * main - prints the numbers from 1 to 100, followed by a new line
- *
- *   * but for multiples of three prints Fizz instead of the number
- *
- *    * and for the multiples of five prints Buzz
- *
- *     * Return: Always 0 (Success)
- *
+ * fizz_buzz - function declaration
+ * Description: prints fizz, buzz or fizbuzz for multiples
+ * of 3 and 5
+ * @range: the end number
+ * Return: void
+ */
+
+void fizz_buzz(int range);
+
+/**
+ * main -entry point
+ * Description: calls fizz_buzz
+ * Return: 0
  */
 
 int main(void)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
-			printf(" FizzBuzz");
-	} else if (i == 1)
-	{
-		printf("%d", i);
-	} else
-	{
-		printf(" %d", i);
-	}
+	fizz_buzz(100);
+	return (0);
 }
-printf("\n");
-return (0);
+
+/**
+ * fizz_buzz - function definition
+ * Description: print fizz or buzz for multiples of 3
+ * and 5
+ * @range: the ending number
+ * Return: void
+ */
+
+void fizz_buzz(int range)
+{
+	int number;
+
+	for (number = 1; number <= range; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (number % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", number);
+		}
+		if (number != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
 }
