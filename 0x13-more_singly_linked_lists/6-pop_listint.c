3c68c5dac28ed1915b5f3c6444142ee852f61645
elias odhiambo <odhiamboelias99@gmail.com>
2023-04-04 13:23:31 -0700

    made the file betty documentation style compliant

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a listint_t linked list, and returns the head node’s data (n)
+ * pop_listint - deletes the head node of a listint_t linked list
  * @head: pointer to the first element in the linked list
  *
- * Return: the data inside the elements hat were deleted or 0 if the list is empty
+ * Return: the data inside the elements hat were deleted
+ * or 0 if the list is empty
  */
 int pop_listint(listint_t **head)
 {
