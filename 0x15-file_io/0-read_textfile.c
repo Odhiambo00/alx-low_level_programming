0f7c8eb645a0fcf835cac363ad4c1b5b9a50fca4
elias odhiambo <odhiamboelias99@gmail.com>
2023-04-07 07:02:49 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_texfile - reads a text file and prints it to standard output
+ * @filename: name of letters to be read and printed
+ * @letters: number of letters to be read and printed
+ *
+ * Rreturn: the number of letters printed or 0 if it failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int r, w;
+	char *bff;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	bff = malloc(sizeof(char) * letters);
+	if (bff == NULL)
+		return (0);
+
+	r = read(fd, bff, letters);
+	return ((r < 0 ? (free(bff), 0) : 1);
+	*(buf + s) = '\0';
+
+	close(fd);
+
+	w = write(STDOUT_FILENO, bff, r);
+	return ((w < 0 ? (free(bff), 0) : (free(bff), t)));
+
+
+
