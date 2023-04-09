c2f564bfe9fa2bea74b8e23f45cdc2514c6bd19b
elias odhiambo <odhiamboelias99@gmail.com>
2023-04-09 04:36:53 -0700

    made 3-set_bit.c Betty compliant

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,11 +5,11 @@
  * @n: pointer to the number to change
  * @index: index of the bit to set to 1
  *
- * Rreturn: 1 for success, -1 for failura
+ * Return: 1 for success, -1 for failura
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int) * 8 -1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
 	*n |= (1UL << index);
