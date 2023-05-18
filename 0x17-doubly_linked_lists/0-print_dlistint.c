af30f540d2f4a7aead9ac6e48b4fee007dae8ac2
Odhiambo00 <odhiamboelias99@gmail.com>
2023-05-18 13:28:04 +0000

    updated 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,9 +6,9 @@
  * Return: Number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 	const dlistint_t *current = h;
 
 	while (current != NULL)
