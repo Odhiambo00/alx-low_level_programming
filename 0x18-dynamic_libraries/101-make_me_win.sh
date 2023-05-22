52d0dceca45e7e23a9e80778e75d34a54eb0a540
Odhiambo00 <odhiamboelias99@gmail.com>
2023-05-22 21:48:56 +0300

    iamawinner.

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Odhiambo00/alx-low_level_programming/
-tree/master/0x18-dynamic_libraries/iamawinner.so
+wget -qP /tmp https://github.com/Odhiambo00/alx-low_level_programming/
+raw/master/0x18-dynamic_libraries/iamawinner.so
 export LD_PRELOAD=/tmp/iamawinner.so
